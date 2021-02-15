b0b3d0b1657db6aaebbcd1b0a7537875319fb1a2
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-02-15 16:27:53 +0200

    added slack api

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/makuzaverite/bd-reminder-bot
 
 go 1.15
+
+require github.com/slack-go/slack v0.8.1
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/slack-go/slack"
+)
 
 func main() {
+
+	api := slack.New("xoxb-1765374764241-1758951736772-8hRkefHN5ZzVZ8dWXBRS5I2y")
+
+	groups, err := api.AuthTest()
+
+	if err != nil {
+		fmt.Println(err)
+		// panic(err)
+	}
+
+	fmt.Println(groups)
+
 	fmt.Println("Hello from birthday Bot")
 }
