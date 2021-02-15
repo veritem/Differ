59551843dee753725f24a732aff273cab38821bc
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-02-15 16:31:13 +0200

    added slack api

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 
 	api := slack.New("xoxb-1765374764241-1758951736772-8hRkefHN5ZzVZ8dWXBRS5I2y")
 
-	groups, err := api.AuthTest()
+	groups, err := api.GetGroups(true)
 
 	if err != nil {
 		fmt.Println(err)
