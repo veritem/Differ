47872c26f14f625dec79d5f772b57eeb2e9b705e
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-02-15 21:45:18 +0200

    sneidng messages done

--- main.go
+++ main.go
@@ -10,14 +10,18 @@ func main() {
 
 	api := slack.New("xoxb-1765374764241-1758951736772-8hRkefHN5ZzVZ8dWXBRS5I2y")
 
-	groups, err := api.GetGroups(true)
+	attachment := slack.Attachment{
+		Pretext: "Happy Birth day Makuza Mugabo Verite",
+		Text:    "Thanks for being cool",
+	}
+
+	channelID, timeStamp, err := api.PostMessage("#general", slack.MsgOptionText("Hello @channel", false), slack.MsgOptionAttachments(attachment), slack.MsgOptionAsUser(true))
 
 	if err != nil {
 		fmt.Println(err)
-		// panic(err)
 	}
 
-	fmt.Println(groups)
+	fmt.Print(channelID, timeStamp)
 
 	fmt.Println("Hello from birthday Bot")
 }
